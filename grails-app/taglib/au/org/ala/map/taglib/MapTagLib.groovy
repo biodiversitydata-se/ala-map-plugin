979dcc0c2c495895ce2a806e621db0ee78897ae5
m-r-c <m-r-c@users.noreply.github.com>
2015-12-08 11:09:15 +1100

    Changes plugin name

--- grails-app/taglib/au/org/ala/map/taglib/MapTagLib.groovy
+++ grails-app/taglib/au/org/ala/map/taglib/MapTagLib.groovy
@@ -14,7 +14,7 @@ class MapTagLib {
      * @attr height The height for the map div. Optional - defaults to {@link #DEFAULT_HEIGHT}
      */
     def map = { attrs ->
-        String leafletImageLocation = "${r.resource(dir: 'vendor/leaflet-0.7.7/images', file: 'marker-icon.png', plugin: 'grails-map').replaceFirst("/marker-icon.png", "")}"
+        String leafletImageLocation = "${r.resource(dir: 'vendor/leaflet-0.7.7/images', file: 'marker-icon.png', plugin: 'ala-map').replaceFirst("/marker-icon.png", "")}"
         String style = "width: ${attrs.width ?: DEFAULT_WIDTH}; height: ${attrs.height ?: DEFAULT_HEIGHT}"
 
         out << "<div id='${attrs.id}' style='${style}' data-leaflet-img='${leafletImageLocation}'></div>"
