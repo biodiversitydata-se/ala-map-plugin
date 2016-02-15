946771df7101f542e4a4bf9e9faf1e153ee210e1
m-r-c <m-r-c@users.noreply.github.com>
2016-02-15 11:16:07 +1100

    Fixes case of Leaflet Draw plugin dependency

--- grails-app/conf/AlaMapResources.groovy
+++ grails-app/conf/AlaMapResources.groovy
@@ -34,8 +34,8 @@ modules = {
 
     leaflet_draw {
         dependsOn "leaflet"
-        resource url: [dir: "vendor/leaflet.draw-0.2.4", file: "leaflet.draw.css", plugin: "ala-map"]
-        resource url: [dir: "vendor/leaflet.draw-0.2.4", file: "leaflet.draw.js", plugin: "ala-map"]
+        resource url: [dir: "vendor/Leaflet.draw-0.2.4", file: "leaflet.draw.css", plugin: "ala-map"]
+        resource url: [dir: "vendor/Leaflet.draw-0.2.4", file: "leaflet.draw.js", plugin: "ala-map"]
     }
 
     leaflet_coords {
