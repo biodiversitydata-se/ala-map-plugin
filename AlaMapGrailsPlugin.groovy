4f4da4b67c82aef4d1f26d78ef999b969d90cabc
m-r-c <m-r-c@users.noreply.github.com>
2016-03-02 16:00:58 +1100

    Changes minimum grails version back to 2.4

--- AlaMapGrailsPlugin.groovy
+++ AlaMapGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class AlaMapGrailsPlugin {
     // the plugin version
     def version = "1.3-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.5 > *"
+    def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
