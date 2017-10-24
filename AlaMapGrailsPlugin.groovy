3f57451003a6c8bee2d6e2ab5450594c964e7b5d
qifeng.bai@csiro.au <q1w2e3r4>
2017-10-24 14:56:18 +1100

    2.0.9 release

--- AlaMapGrailsPlugin.groovy
+++ AlaMapGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AlaMapGrailsPlugin {
     // the plugin version
-    def version = "2.0.9-SNAPSHOT"
+    def version = "2.0.9"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
